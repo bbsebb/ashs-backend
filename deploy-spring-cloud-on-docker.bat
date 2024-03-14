f6ffbdc827daae822e99ad755067f6bc43cb8888
Sébastien Burckhardt <sebastien.burckhardt@gmail.com>
2024-03-14 01:02:27 +0100

    add docker-compose for spring cloud deployment in local

---
+++ deploy-spring-cloud-on-docker.bat
@@ -0,0 +1,35 @@
+@echo off
+SETLOCAL EnableDelayedExpansion
+
+:: Chemin vers le dossier de votre projet backend
+cd C:\Users\bbseb\Documents\programmation\ashs\backend
+
+:: Etape 1: Commit et push sur GitHub
+cd config-repo
+echo Commit et push des modifications sur GitHub...
+git add .
+git commit -m "Mise à jour des services"
+git push
+cd ..
+echo %cd%
+
+:: Etape 2: Build des services avec Gradle
+echo Construction des services Spring Boot avec Gradle...
+FOR /D %%d IN (*) DO (
+    IF EXIST %%d\build.gradle (
+        cd %%d
+        echo Building %%d...
+        call .\gradlew build
+        cd ..
+    )
+)
+
+
+:: Etape 3: Build et lancement des containers avec Docker Compose
+echo Construction et lancement des containers Docker...
+docker-compose down
+docker-compose up --build -d
+
+echo Processus termine!
+pause
+ENDLOCAL
